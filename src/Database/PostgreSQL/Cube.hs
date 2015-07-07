eaea0008e9dc6e8b4ee64e1c050c07a5475729e0
inaimathi <inaimathi@self>
2015-07-07 09:08:24 -0400

    Initial commit

---
+++ src/Database/PostgreSQL/Cube.hs
@@ -0,0 +1,18 @@
+module Database.PostgreSQL.Cube (Cube(..))where
+
+import Database.PostgreSQL.Simple.ToField
+
+import Data.List (intersperse, intercalate)
+import Data.ByteString.Builder (byteString, char8)
+
+data Cube = Cube [[Float]]
+
+instance ToField Cube where
+    toField (Cube cs) = Many $
+                        Plain (byteString "CUBE('") :
+                        (intercalate [comma] $ map point cs) ++
+                        [Plain (byteString "')")]
+                            where comma = Plain (char8 ',')
+                                  point lst = Plain (char8 '(') :
+                                              (intersperse comma $ map toField lst) ++
+                                              [Plain (char8 ')')]
